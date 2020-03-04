22b935a3d1e3a5f9eab8a5fdc45b881db08927f6
Briceson Roy <royzone3@ymail.com>
2020-03-04 14:05:50 -0500

    added Javascript link

---
+++ linkscript.js
@@ -0,0 +1,27 @@
+//nav links
+
+//Home 
+$('.home').on('click', 'button', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    })
+});
+//About me
+$('.about').on('click', 'button', function (event) {
+    event.preventDefault();
+    $('').animate
+});
+//Skills
+$('.skills').on('click', 'button', function (event) {
+    event.preventDefault();
+
+});
+//Projects
+$('.projects').on('click', 'button', function (event) {
+    event.preventDefault();
+});
+//Contacts
+$('.contact').on('click', 'button', function (event) {
+    event.preventDefault();
+});
